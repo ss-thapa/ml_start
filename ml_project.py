e3c5714bc21e9767e23f0fa91e59c551ea3ce21e
ss-thapa <145964403+ss-thapa@users.noreply.github.com>
2024-01-22 13:25:18 +0545

    initilize project

---
+++ ml_project.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+
+
+df = pd.read_csv('/Users/sunilthapa/Desktop/programming/Final_work space/My_projects/ML_intro/placement.csv')
+
+
+
+
+print(df.head())
+
+
+
+
+
